07c6875201fc372e1cf295810bab4f1e6ef35fdc
Simon Wood <simon.wood@holidayextras.com>
2014-08-14 13:14:20 +0100

    Now calling binlist.net and remapping to our schema

--- test/index.js
+++ test/index.js
@@ -5,12 +5,39 @@ var should = require( 'chai' ).should();
 var IinChecker = require( '../index' );
 var iin = new IinChecker( {} );
 
-// Before each Mocha - Look tomorrow
+describe( '#lookup a card', function() {
+	var testGenCard;
+	it( 'should lookup a card without error', function( done ) {
+		iin.lookup( '411111', function( err, result ) {
+			if ( err ) {
+				throw( err );
+			} else {
+				testGenCard = result;
+				done();
+			}
+		} );
+	} );
+	
+	it( 'iin lookup returns card as an object', function( done ) {
+		testGenCard.should.be.a( 'object' );
+		done();
+	} );
 
+	it( 'card object returned contains the correct properties', function( done ) {
+		testGenCard.should.have.property( 'bin' );
+		testGenCard.should.have.property( 'brand' );
+		testGenCard.should.have.property( 'issuer' );
+		testGenCard.should.have.property( 'type' );
+		testGenCard.should.have.property( 'category' );
+		testGenCard.should.have.property( 'country' );
+		done();
+	} );
+} );
+
+	
 describe( '#lookup visa debit', function() {
 	var testVisaDebitCard;
 	it( 'should lookup a card without error', function( done ) {
-		
 		iin.lookup( '431940', function( err, result ) {
 			if ( err ) {
 				throw( err );
@@ -21,21 +48,6 @@ describe( '#lookup visa debit', function() {
 		} );
 	} );
 	
-	it( 'iin lookup returns card as an object', function( done ) {
-		testVisaDebitCard.should.be.a( 'object' );
-		done();
-	} );
-
-	it( 'card object returned contains the correct properties', function( done ) {
-		testVisaDebitCard.should.have.property( 'bin' );
-		testVisaDebitCard.should.have.property( 'brand' );
-		testVisaDebitCard.should.have.property( 'issuer' );
-		testVisaDebitCard.should.have.property( 'type' );
-		testVisaDebitCard.should.have.property( 'category' );
-		testVisaDebitCard.should.have.property( 'country' );
-		done();
-	} );
-
 	it( 'card is of type debit', function( done ) {
 		testVisaDebitCard.type.should.equal( iin.types.DEBIT );
 		done();
@@ -48,6 +60,25 @@ describe( '#lookup visa debit', function() {
 } );
 
 describe( '#lookup mastercard credit', function() {
-	iin.lookup( '518791', function( err, testVisaDebitCard ) {
+	var testMasterCreditCard;
+	it( 'should lookup a card without error', function( done ) {
+		iin.lookup( '518791', function( err, result ) {
+			if ( err ) {
+				throw( err );
+			} else {
+				testMasterCreditCard = result;
+				done();
+			}
+		} );
+	} );
+		
+	it( 'card is of type credit', function( done ) {
+		testMasterCreditCard.type.should.equal( iin.types.CREDIT );
+		done();
+	} );
+
+	it( 'card is of brand mastercard', function( done ) {
+		testMasterCreditCard.brand.should.equal( iin.brands.MASTERCARD );
+		done();
 	} );
 } );
