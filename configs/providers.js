73e3a977b74866324c37961376e1f2daef4d154c
Kevin Hodges <kev_hodges_1@hotmail.com>
2015-07-23 12:58:08 +0100

    switch provider order to temporarily fix some bin range holes

--- configs/providers.js
+++ configs/providers.js
@@ -1,20 +1,5 @@
 module.exports = [
 	{
-		name: "BINLIST",
-		domain: "http://www.binlist.net",
-		path: "/json/",
-		map: function( returnedData, nullValue ) {
-			return {
-				iin: returnedData.bin,
-				brand: returnedData.brand,
-				issuer: returnedData.bank,
-				type: returnedData.card_type,
-				category: returnedData.card_category,
-				country: returnedData.country_code
-			}
-		}
-	},
-	{
 		name: "RIBBON",
 		domain: "https://bins.ribbon.co",
 		path: "/api/v1/bins/",
@@ -25,6 +10,21 @@ module.exports = [
 				issuer: returnedData.issuer,
 				type: ( returnedData.type ? returnedData.type : nullValue ),
 				category: nullValue,
+				country: returnedData.country_code
+			}
+		}
+	},
+	{
+		name: "BINLIST",
+		domain: "http://www.binlist.net",
+		path: "/json/",
+		map: function( returnedData, nullValue ) {
+			return {
+				iin: returnedData.bin,
+				brand: returnedData.brand,
+				issuer: returnedData.bank,
+				type: returnedData.card_type,
+				category: returnedData.card_category,
 				country: returnedData.country_code
 			}
 		}
