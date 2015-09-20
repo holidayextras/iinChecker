6fc6c41769396f40e668901f028f223a9c25185c
Rob Huzzey <robhuzzey@users.noreply.github.com>
2015-09-20 17:16:47 +0100

    Ripped out ribbon
    Expedite issue where ribbon was failing

--- configs/providers.js
+++ configs/providers.js
@@ -1,20 +1,5 @@
 module.exports = [
 	{
-		name: "RIBBON",
-		domain: "https://bins.ribbon.co",
-		path: "/api/v1/bins/",
-		map: function( returnedData, nullValue ) {
-			return {
-				iin: returnedData.bin,
-				brand: returnedData.brand,
-				issuer: returnedData.issuer,
-				type: ( returnedData.type ? returnedData.type : nullValue ),
-				category: nullValue,
-				country: returnedData.country_code
-			}
-		}
-	},
-	{
 		name: "BINLIST",
 		domain: "http://www.binlist.net",
 		path: "/json/",
